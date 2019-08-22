2c08926fa0c899ad1724588e43e916b034cf3e4e
Alec Marcus <git@alecmarc.us>
2019-08-21 23:54:07 -0400

    add manual input to slider

--- src/components/Controls/Slider/index.js
+++ src/components/Controls/Slider/index.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef, useState, useMemo } from 'react';
 import PropTypes from 'prop-types';
+import { className } from 'utils';
 import styles from './Slider.module.scss';
 
 const Slider = ({
@@ -17,12 +18,23 @@ const Slider = ({
   });
 
   const handleChange = e => {
-    setValue(e.target.value);
+    const inputValue = parseInt(e.target.value);
+    const cleanValue = inputValue > max ? max : inputValue < min ? min : inputValue;
+    setValue(cleanValue);
   };
 
   return (
-    <div >
-      <div className={styles.sliderWrapper}>
+    <div {...className(styles.sliderOuterWrapper, (value === min) && styles.minned, (value === max) && styles.maxxed)}>
+      <input
+          className={styles.value}
+          type="number"
+          max={max}
+          min={min}
+          onChange={handleChange}
+          value={value}
+          placeholder={value}
+        />
+      <div className={styles.sliderInnerWrapper}>
         <span className={styles.fill} ref={fillRef} style={fillWidth}/>
         <input type="range"
           className={styles.slider}
@@ -35,7 +47,7 @@ const Slider = ({
           value={value}
           />
       </div>
-      <label htmlFor={property}>{labelText}</label>
+      <label className={styles.label} htmlFor={property}>{labelText}</label>
     </div>
   );
 };
